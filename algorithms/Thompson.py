3fdb9be6963d4fd02b661313372c5eb46d446e25
Henry Vu <henryvu@Henrys-MacBook-Pro.local>
2024-04-05 21:35:43 -0600

    added whittle index

--- algorithms/Thompson.py
+++ algorithms/Thompson.py
@@ -2,36 +2,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 np.set_printoptions(suppress=True, precision=2)
 np.random.seed(32)
-
 # Bernoulli reward with parameter theta
 def reward(arm, reward_prob):
     return np.random.binomial(n=1, p=reward_prob[arm])
 
 # Beta prior
-def TS(num_arms, horizon, alpha, beta, reward_prob):
+def TS(num_arms, horizon, reward_prob, best_arm):
+    regrets = np.zeros(horizon)
     samples = np.zeros((horizon, num_arms))
     rewards = np.zeros(horizon)
+    alphas = np.zeros((horizon+1, num_arms))
+    betas = np.zeros((horizon+1, num_arms))
+    alpha = np.ones(num_arms)
+    beta = np.ones(num_arms)
     for t in range(horizon):
+        alphas[t]= alpha
+        betas[t] = beta
         for i in range(num_arms):
-            samples[t, i] = np.random.beta(alpha[t,i], beta[t,i])
-        chosen_arm = np.argmax(samples)
-        reward = reward(chosen_arm, reward_prob)
-        alpha[t+1,i] = alpha[t,i]+reward
-        beta[t+1,i] = beta[t,i]+1-reward
-        rewards[t] = reward
+            samples[t, i] = np.random.beta(alpha[i], beta[i])
+        chosen_arm = np.argmax(samples[t,:])
+        x = reward(chosen_arm, reward_prob)
+        regrets[t] = reward_prob[best_arm] - reward_prob[chosen_arm]
+        alpha[chosen_arm] += x
+        beta[chosen_arm] += 1-x
+        rewards[t] = x
+        
+    alphas[horizon] = alpha
+    betas[horizon] = beta
+    
+    return samples, alphas, betas, rewards, regrets
 
-    return samples, alpha, beta, rewards
-reward_prob = [0.1, 0.9, 0.2, 0.3, 0.15]
-best_reward = max(reward_prob)
-num_arms = 5
-horizon = 2000
-alpha = np.ones((horizon+1, num_arms))
-beta = np.ones((horizon+1, num_arms))
+reward_prob = [0.9, 0.5, 0.2]
+best_arm = 0
+num_arms = 3
+horizon = 5000
 
-samples, alpha, beta, rewards = TS(num_arms, horizon, alpha, beta, reward_prob)
+samples, alpha, beta, rewards, regrets = TS(num_arms, horizon, reward_prob, best_arm)
+
 thetas = alpha/(alpha + beta)
 # Cum reward over time
 plt.figure(figsize=(10, 6))
+
 for arm in range(num_arms):
     plt.plot(thetas[:, arm], label=f'Arm {arm+1}')
 plt.xlabel('Time step')
@@ -41,11 +52,9 @@ plt.legend()
 plt.show()
 #-------------------------------------------------------
 # Regret over time
-ts_sum = np.cumsum(rewards)
-ts_regret = (np.arange(horizon)*best_reward - ts_sum)
 plt.figure(figsize=(10, 6))
-plt.plot(ts_regret, label='Thompson Sampling Regret')
+plt.plot(regrets, label='Thompson Sampling Regret')
 plt.legend()
 plt.xlabel('Time step')
-plt.ylabel('Cumulative Regret')
-plt.title('Cumulative Regret of Thompson Sampling')
+plt.ylabel('Per-step Regret')
+plt.title('Regret of Thompson Sampling')
